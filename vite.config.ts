786a6451ee3e285c8497192edf926993d98ebd14
Jeszpy <hleb.lukashonak@gmail.com>
2024-04-11 13:04:02 +0300

    add 144px image

--- vite.config.ts
+++ vite.config.ts
@@ -4,35 +4,41 @@ import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
 const manifestForPlugIn: Partial<VitePWAOptions> = {
   registerType: 'prompt',
-  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
     name: "React-vite-app",
     short_name: "react-vite-app",
     description: "I am a simple vite app",
-    icons: [{
-      src: '/android-chrome-192x192.png',
-      sizes: '192x192',
-      type: 'image/png',
-      purpose: 'favicon'
-    },
-    {
-      src: '/android-chrome-512x512.png',
-      sizes: '512x512',
-      type: 'image/png',
-      purpose: 'favicon'
-    },
-    {
-      src: '/apple-touch-icon.png',
-      sizes: '180x180',
-      type: 'image/png',
-      purpose: 'apple touch icon',
-    },
-    {
-      src: '/maskable_icon.png',
-      sizes: '512x512',
-      type: 'image/png',
-      purpose: 'any maskable',
-    }
+    icons: [
+      {
+        src: "/dimensions-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      }, {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      }
     ],
     theme_color: "#317EFB",
     background_color: '#f0e7db',
