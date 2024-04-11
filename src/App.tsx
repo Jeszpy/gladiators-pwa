0f53c3cef8b9adb44d2499a160fd8a496f6d53e7
Jeszpy <hleb.lukashonak@gmail.com>
2024-04-11 13:08:32 +0300

    change index.html

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>Hello from GLADIATORS ONLINE</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
